8f147c2ef48f013660270364d3c35ffc9af86d6b
loy <loydjoshwad@gmail.com>
2026-07-30 17:46:46 +0530

    update

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -9,28 +9,28 @@ type UserService struct {
 	repo *repository.UserRepository
 }
 
-func NewUserService(repo *repository.UserRepository) *UserService {
+func NewUserService(repo *repository.UserRepository) *UserService{
 	return &UserService{
 		repo: repo,
 	}
 }
 
-func (s *UserService) Create(user *model.User) error {
+func (s *UserService) Create (user *model.User) error{
 	return s.repo.Create(user)
 }
 
-func (s *UserService) GetAll() ([]model.User, error) {
+func (s *UserService) GetAll() ([]model.User,error){
 	return s.repo.GetAll()
 }
 
-func (s *UserService) GetByID(id uint) (*model.User, error) {
+func (s *UserService) GetByID(id uint) (*model.User,error){
 	return s.repo.GetByID(id)
 }
 
-func (s *UserService) Update(user *model.User) error {
+func (s *UserService) Update(user *model.User) error{
 	return s.repo.Update(user)
 }
 
-func (s *UserService) Delete(id uint) error {
+func (s *UserService) Delete(id uint) error{
 	return s.repo.Delete(id)
 }
